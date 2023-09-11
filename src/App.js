862b3b4209d3f9f757ed6ed24daaa859717eda39
Gokulcoder-99 <gokulkrishnan072@gmail.com>
2023-09-11 13:04:17 +0530

    3

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import { setUser } from "./store/auth.js";
 
 function App() {
   const token = Cookies.get("token");
-  const [isLoading, setIsLoading] = useState(true);
+
   const dispatch = useDispatch();
 
   async function fetchUser() {
-    setIsLoading(true);
     const res = await fetch(`${process.env.REACT_APP_API_URL}/user`, {
       headers: {
         Authorization: `Bearer ${token}`,
@@ -22,16 +21,11 @@ function App() {
       const user = await res.json();
       dispatch(setUser(user));
     }
-    setIsLoading(false);
   }
 
   useEffect(() => {
     fetchUser();
   }, []);
-
-  if (isLoading) {
-    return <p>Loading ...</p>;
-  }
 
   return (
     <>
